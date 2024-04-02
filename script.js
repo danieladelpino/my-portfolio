4cb2aa976ee4753a5ac0f3b653f5f31ddb36e72f
Daniela del Pino <danielaabigaildelpino@gmail.com>
2024-04-02 16:19:54 -0300

    añadimos estilos mobile y tablet

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 // TypeText
 
-    let i = 0;
-    let text = "Desarrolladora Front End";
-    let speed = 100;
+let i = 0;
+let text = "Desarrolladora Front End";
+let speed = 100;
 
-    const typeWrite = () => {
-        if (i < text.length) {
-            document.querySelector('.hero-title').innerHTML += text.charAt(i);
-            i++;
-            setTimeout(typeWrite, speed);
-        } else {
-            i = 0;
-            document.querySelector('.hero-title').innerHTML = ''; 
-            setTimeout(typeWrite, speed);
-        }
+const typeWrite = () => {
+    if (i < text.length) {
+        document.querySelector('.hero-title').innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWrite, speed);
+    } else {
+        i = 0;
+        document.querySelector('.hero-title').innerHTML = '';
+        setTimeout(typeWrite, speed);
     }
+}
 
-    typeWrite();
+typeWrite();
 
 // Librería
 
 new fullpage('#fullpage', {
     autoScrolling: true,
     scrollHorizontally: true,
-    sectionsColor : ['#262451', '#10151d', '#10151d', '#10151d'],
+    sectionsColor: ['#262451', '#10151d', '#10151d', '#10151d'],
     scrollOverflow: true,
     scrollOverflowOptions: {
-        click: true, // Permite hacer clic en los enlaces dentro de la sección scrollable
-        scrollbars: true, // Muestra las barras de desplazamiento cuando sea necesario
-        mouseWheel: true, // Habilita el desplazamiento con la rueda del mouse dentro de la sección
-        interactiveScrollbars: true // Permite arrastrar las barras de desplazamiento
+        click: true,
+        scrollbars: true,
+        mouseWheel: true,
+        interactiveScrollbars: true
     }
 });
 
@@ -37,7 +37,7 @@ var granimInstance = new Granim({
     element: '#canvas-basic',
     direction: 'left-right',
     isPausedWhenNotInView: true,
-    states : {
+    states: {
         "default-state": {
             gradients: [
                 ['#ff9966', '#ff5e62'],
